3674521c19e22b1ecaca2ec004e197780e01fc42
sapnil1482 <91736247+sapnil1482@users.noreply.github.com>
2022-02-26 14:15:48 +0530

    Add files via upload

---
+++ Prac-3.py
@@ -0,0 +1,15 @@
+# Date:-27/1/2022
+# Author Sapnil Patel
+# Github link:- https://github.com/sapnil1482/Python_Ass.git
+N = int(input())
+
+storage = map(int, input().split())
+storage = sorted(storage)
+
+for i in range(len(storage)):
+    if(i != len(storage)-1):
+        if(storage[i]!=storage[i-1] and storage[i]!=storage[i+1]):
+            print(storage[i])
+            break;
+    else:
+        print(storage[i])
